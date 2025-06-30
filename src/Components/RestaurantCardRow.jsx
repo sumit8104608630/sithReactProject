acb5ae7e97659b7d539ded927be11274824c406c
sumit sharma <114969811+sumit8104608630@users.noreply.github.com>
2025-06-30 19:44:54 +0530

    dynamic data

--- src/Components/RestaurantCardRow.jsx
+++ src/Components/RestaurantCardRow.jsx
@@ -10,7 +10,6 @@ function RestaurantCardRow() {
     try {
       const data = await fetch(cardUrl);
       const json = await data.json();
-      console.log("json",json?.data?.cards[1].card.card?.gridElements?.infoWithStyle?.restaurants);
       setRestaurantRowData(json?.data?.cards[1].card.card?.gridElements?.infoWithStyle?.restaurants)
     }
     catch (err) {
--- src/Util/constant.js
+++ src/Util/constant.js
@@ -1,3 +1,3 @@
-export const cardUrl="https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=19.10205&lng=72.9087744&carousel=true&third_party_vendor=1"
+export const cardUrl="https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.10205&lng=72.9087744&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 export const firstRowImageConstantUrl="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/"
 export const restaurantImagesConstUrl="https://media-assets.swiggy.com/swiggy/image/upload/"
